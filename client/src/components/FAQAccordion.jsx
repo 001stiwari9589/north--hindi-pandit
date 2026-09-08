3108eff3fd8dc7cacc4e4a8a57ee6d8d9f0bb98d
001stiwari9589 <001stiwari9589@gmail.com>
2026-09-09 00:30:52 +0530

    fix(faq): lighten question font weight to 500 and remove bottom contact helpline card

--- client/src/components/FAQAccordion.jsx
+++ client/src/components/FAQAccordion.jsx
@@ -226,27 +226,6 @@ export default function FAQAccordion({ currentLang = 'en' }) {
             );
           })}
         </div>
-
-        {/* Bottom Help CTA */}
-        <div className="faq-footer-help">
-          <span className="faq-footer-help-text">{info.moreQuestions}</span>
-          <a
-            href="tel:+919589018011"
-            className="faq-footer-call-btn"
-            title="Direct Call to Acharya Ji"
-          >
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              style={{ flexShrink: 0 }}
-            >
-              <path d="M6.62 10.79a15.05 15.05 0 006.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
-            </svg>
-            <span>{info.callAcharya}: +91 95890 18011</span>
-          </a>
-        </div>
       </div>
     </section>
   );
